b8d3ee39f62560ab8806cf9fcfb3ae4f5244aa16
denis <proteichik@gmail.com>
2016-11-29 00:18:16 +0300

    add create user command

---
+++ app/Console/Commands/CreateUser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Cerbero\CommandValidator\ValidatesInput;
+use App\User;
+use Auth;
+
+class CreateUser extends Command
+{
+    use ValidatesInput;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:create {name} {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create user command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    protected function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6',
+        ];
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $user = User::create([
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => bcrypt($this->argument('password')),
+        ]);
+
+        $this->info(sprintf('User created. Id: %s', $user->id));
+    }
+}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,7 @@
                     <input type="text" name="email" value="{{ old('email') }}">
                     <h5>Password:</h5>
                     <input id="password" type="password" name="password" required>
+                    <input type="checkbox" name="remember"> Запомнить меня
                     <input type="submit" value="Login">
                 </form>
             </div>
