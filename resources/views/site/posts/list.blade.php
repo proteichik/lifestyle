9f0c4b86c656abee4ecf5f0913555ed8e96f0ff3
proteichik <proteihik@gmail.com>
2017-11-12 16:35:21 +0100

    add counter to categories

--- resources/views/site/posts/list.blade.php
+++ resources/views/site/posts/list.blade.php
@@ -39,7 +39,7 @@
                 <h3>Категории</h3>
                 <ul>
                     @foreach($categories as $category)
-                        <li><a href="{{ route('site.posts.by_category', [$category->id]) }}">{{ $category->name }} <span class="badge badge-pill badge-secondary">{{  $category->posts()->wherePublish(1)->count()}}</span></a></li>
+                        <li><a href="{{ route('site.posts.by_category', [$category->id]) }}">{{ $category->name }} </a> <span class="label label-danger pull-right">{{  $category->posts()->wherePublish(1)->count()}}</span></li>
                     @endforeach
                 </ul>
             </div>
